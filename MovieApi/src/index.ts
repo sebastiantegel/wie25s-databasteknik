74233c9df8814e53f4335f2f9f1d42d3b4e9d71d
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2026-01-08 10:55:50 +0100

    Moved connection to env file Updated logic to connect

--- MovieApi/src/index.ts
+++ MovieApi/src/index.ts
@@ -1,5 +1,15 @@
+import { configDotenv } from "dotenv";
 import mongoose from "mongoose";
 
-mongoose.connect(
-  "mongodb+srv://sebastiantegel:UsMeHBSPvUpUoSs2@cluster0.a2ub8.mongodb.net/omdbLight?retryWrites=true&w=majority"
-);
+configDotenv();
+
+const mongoUri = process.env.MONGO_URI;
+
+if (!mongoUri) {
+  throw new Error("MONGO_URI is not set in .env");
+}
+
+mongoose
+  .connect(mongoUri)
+  .then(() => console.log("Connected to mongo db"))
+  .catch((err) => console.error("Error connecting to mongo db", err));
