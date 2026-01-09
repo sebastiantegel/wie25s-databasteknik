f222059ce4936be4d2001566b2269e2feebd786a
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2026-01-09 08:25:37 +0100

    Updated with async/await

--- MovieApi/src/index.ts
+++ MovieApi/src/index.ts
@@ -1,5 +1,6 @@
 import { configDotenv } from "dotenv";
 import mongoose from "mongoose";
+import { movieModel } from "./models/Movie.js";
 
 configDotenv();
 
@@ -9,7 +10,9 @@ if (!mongoUri) {
   throw new Error("MONGO_URI is not set in .env");
 }
 
-mongoose
-  .connect(mongoUri)
-  .then(() => console.log("Connected to mongo db"))
-  .catch((err) => console.error("Error connecting to mongo db", err));
+try {
+  await mongoose.connect(mongoUri);
+  console.log("Connected to mongo db");
+} catch (err) {
+  console.error("Error connecting to mongo db", err);
+}
